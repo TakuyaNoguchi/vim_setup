c242270252f93132dbac6d3ab530a345d6c4732c
Takuya Noguchi <takuya@netlab.jp>
2016-10-29 20:40:19 +0900

    設定ファイルの配置スクリプトを作成。

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+
+if [ ! -e "$HOME/.vimrc" ]; then
+  ln -sf $(git rev-parse --show-toplevel)/dot.vimrc $HOME/.vimrc
+fi
+
+if [ ! -e "$HOME/.vim" ]; then
+  mkdir -p "$HOME/.vim"
+fi
+
+if [ ! -e "$HOME/.vim/indent" ]; then
+  ln -sfn $(git rev-parse --show-toplevel)/indent $HOME/.vim/indent
+fi
