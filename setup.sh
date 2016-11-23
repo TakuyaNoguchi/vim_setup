4e28649518bf2caa3c9eec5e94b78f099e3ae77d
Takuya Noguchi <takuya@netlab.jp>
2016-11-23 21:14:18 +0900

    dot.vimrc の配置先を .vimディレクトリ以下に変更

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 
 set -eu
 
-if [ ! -e "$HOME/.vimrc" ]; then
-  ln -sf $(git rev-parse --show-toplevel)/dot.vimrc $HOME/.vimrc
-fi
-
 if [ ! -e "$HOME/.vim" ]; then
   mkdir -p "$HOME/.vim"
+fi
+
+if [ ! -e "$HOME/.vim/vimrc" ]; then
+  ln -sf $(git rev-parse --show-toplevel)/dot.vimrc $HOME/.vim/vimrc
 fi
 
 if [ ! -e "$HOME/.vim/filetype.vim" ]; then
